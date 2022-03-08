6405d37d16646dbc54557951f8a39a09e38982fc
JaroschJr <jaroschjr@gmail.com>
2022-03-08 14:00:18 -0600

    Modified order to include Buyer and Appliance. Created Backorder, a descendant of Order

---
+++ Backorder.java
@@ -0,0 +1,9 @@
+
+public class Backorder extends Order {
+
+	public Backorder(int orderID, double orderPrice, int quantity, Customer orderCustomer, Appliance setAppliance) {
+		super(orderID, orderPrice, quantity, orderCustomer, setAppliance);
+		// TODO Auto-generated constructor stub
+	}
+
+}
--- Order.java
+++ Order.java
@@ -2,19 +2,63 @@ public class Order {
     private int orderID;
     private double orderPrice;
     private int quantity;
+    private Customer buyer;
+    private Appliance appliance;
 
-    public Order(int orderID, double orderPrice, int quantity) {
-        this.orderID = orderID;
-        this.orderPrice = orderPrice;
-        this.quantity = quantity;
+    public Order(int orderID, double orderPrice, int quantity, Customer orderCustomer, Appliance setAppliance) {
+        this.setOrderID(orderID);
+        this.setOrderPrice(orderPrice);
+        this.setQuantity(quantity);
+        this.setBuyer(orderCustomer);
+        this.setAppliance(setAppliance);
     }// end public Order class
 
     public void setPrice(double newPrice) {
-        orderPrice = newPrice;
+        setOrderPrice(newPrice);
     }// end setOrder
 
     public void changeQuantity(int newQuantity){
-        quantity = newQuantity;
+        setQuantity(newQuantity);
     }// end changeQuantity
+
+	public int getOrderID() {
+		return orderID;
+	}
+
+	public void setOrderID(int orderID) {
+		this.orderID = orderID;
+	}
+
+	public double getOrderPrice() {
+		return orderPrice;
+	}
+
+	public void setOrderPrice(double orderPrice) {
+		this.orderPrice = orderPrice;
+	}
+
+	public int getQuantity() {
+		return quantity;
+	}
+
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
+
+	public Customer getBuyer() {
+		return buyer;
+	}
+
+	public void setBuyer(Customer buyer) {
+		this.buyer = buyer;
+	}
+
+	public Appliance getAppliance() {
+		return appliance;
+	}
+
+	public void setAppliance(Appliance appliance) {
+		this.appliance = appliance;
+	}
     
 } // end class Order
