7abe44803d5728580fb918f66c2a12cf076349c5
Tai Vu <vutai866@gmail.com>
2022-03-08 12:15:35 -0600

    Customer.java

---
+++ Customer.java
@@ -0,0 +1,12 @@
+public class Customer {
+    private int customerID;
+    private String customerName;
+    private String customerAddress;
+    private int customerPhoneNumber;
+
+    this.customerID = customerID;
+    this.customerName = customerName;
+    this.customerAddress = customerAddress;
+    this.customerPhoneNumber = customerPhoneNumber;
+
+}
