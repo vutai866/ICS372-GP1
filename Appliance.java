adfcfd22e285c23fde8ffa2444abb79d7e6ea54f
JaroschJr <jaroschjr@gmail.com>
2022-03-08 13:06:06 -0600

    The skeleton of Apliance has been made. You may use or modify it now.

--- Appliance.java
+++ Appliance.java
@@ -1,4 +1,39 @@
-
 public class Appliance {
+	private int applianceID;
+	private String applianceName;
+	private String brandName;
+	private double price;
+	private int quantity;
+	
+	public int getApplianceID() {
+		return applianceID;
+	}
+	public void setApplianceID(int applianceID) {
+		this.applianceID = applianceID;
+	}
+	public String getApplianceName() {
+		return applianceName;
+	}
+	public void setApplianceName(String applianceName) {
+		this.applianceName = applianceName;
+	}
+	public String getBrandName() {
+		return brandName;
+	}
+	public void setBrandName(String brandName) {
+		this.brandName = brandName;
+	}
+	public double getPrice() {
+		return price;
+	}
+	public void setPrice(double price) {
+		this.price = price;
+	}
+	public int getQuantity() {
+		return quantity;
+	}
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
 
 }
