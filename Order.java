bb084e226d8e7a84f4f69339416d9153f58caf44
Tai Vu <vutai866@gmail.com>
2022-03-08 12:44:25 -0600

    edit Order

--- Order.java
+++ Order.java
@@ -2,4 +2,19 @@ public class Order {
     private int orderID;
     private double orderPrice;
     private int quantity;
-}
+
+    public Order(int orderID, double orderPrice, int quantity) {
+        this.orderID = orderID;
+        this.orderPrice = orderPrice;
+        this.quantity = quantity;
+    }// end public Order class
+
+    public void setPrice(double newPrice) {
+        orderPrice = newPrice;
+    }// end setOrder
+
+    public void changeQuantity(int newQuantity){
+        quantity = newQuantity;
+    }// end changeQuantity
+    
+} // end class Order
