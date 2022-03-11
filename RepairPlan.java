aa55934572b06ae5b7125c07881e019a1d573ba5
Tai Vu <vutai866@gmail.com>
2022-03-10 22:08:38 -0600

    Add Repair plan

---
+++ RepairPlan.java
@@ -0,0 +1,10 @@
+
+public class RepairPlan extends Customer{
+
+    private double repairCost;
+
+    public RepairPlan(int customerID, String customerName, Appliance getApplianceName) {
+        super(customerID, customerName, getApplianceName);
+        // TODO Auto-generated constructor stub
+    }
+}
