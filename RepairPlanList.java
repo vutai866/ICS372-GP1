63165048497d7e723c495f106808b197cfbcfec5
funkybunny2 <100392730+funkybunny2@users.noreply.github.com>
2022-03-12 12:10:52 -0600

    Update RepairPlanList.java

--- RepairPlanList.java
+++ RepairPlanList.java
@@ -3,20 +3,20 @@ import java.util.Iterator;
 import java.util.List;
 
 public class RepairPlanList implements Iterable<RepairPlan>{
-    private List<RepairPlan> RepairPlans = new ArrayList<>();
+    private List<RepairPlan> repairPlans = new ArrayList<>();
 
     public boolean insertRepairPlan(RepairPlan RepairPlan){
-        RepairPlans.add(RepairPlan);
+        repairPlans.add(RepairPlan);
         return true;
     }
 
     public List<RepairPlan> getRepairPlans() {
-        return RepairPlans;
+        return repairPlans;
     }
 
     @Override
     public Iterator<RepairPlan> iterator() {
         // TODO Auto-generated method stub
-        return RepairPlans.iterator();
+        return repairPlans.iterator();
     }
 } // end RepairPlanList
