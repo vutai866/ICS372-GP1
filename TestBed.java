22275baec9b5803499082febfa5be91ffb2c7ec6
funkybunny2 <100392730+funkybunny2@users.noreply.github.com>
2022-03-18 17:57:04 -0500

    Update TestBed.java

--- TestBed.java
+++ TestBed.java
@@ -8,16 +8,23 @@ import java.util.Random;
 
 public class TestBed {
 	ShopSystem shop;
+	FacadeInterface facade;
 
 	public TestBed(ShopSystem shop) {
 		super();
 		this.shop = shop;
+		this.facade = new FacadeInterface(this.shop);
 	}
 
 	public void test() {
-		// using assert statement we check our result from our userinterface to see if
-		// they are correct
-		// our userinterface don't return anything so i dont know how we can check.
+
+		facade.addToInventory();
+		facade.purchase();
+		facade.enrollCustomer();
+		facade.chargeAllRepairPlans();
+		facade.fulfillBackorder();
+		facade.withdrawCustomer();
+		facade.printTotalRevenue();
 
 	}
 
@@ -28,6 +35,7 @@ public class TestBed {
 		Customer jarosch = new Customer("jarosch", "200 16th ave n MPLS MN", "6126239743");
 		Customer boston = new Customer("boston", "1234 15th ave N Mpls MN", "6129372812");
 		Customer john = new Customer("john", "20 17th ave N MPLS MN", "6120284732");
+
 		shop.getCustomers().insertCustomer(boston);
 		shop.getCustomers().insertCustomer(chee);
 		shop.getCustomers().insertCustomer(tai);
@@ -36,25 +44,29 @@ public class TestBed {
 	}
 
 	public void makeAppliances() {
-		for (int i = 0; i > 20; i++) {
+		for (int i = 0; i < 20; i++) {
 			int rand = new Random().nextInt(4);
 			double price = new Random().nextInt(9999);
 			if (rand == 0) {
 				int repairPlanCost = new Random().nextInt(1000);
 				Appliance appliance = new Washer("washer" + i, "sears" + i, price, repairPlanCost);
+				appliance.setQuantity(5);
 				shop.getAppliances().insertAppliance(appliance);
 			} else if (rand == 1) {
 				int repairPlanCost = new Random().nextInt(1000);
 				Appliance appliance = new Dryer("dryer" + i, "drya20s" + i, price, repairPlanCost);
+				appliance.setQuantity(5);
 				shop.getAppliances().insertAppliance(appliance);
 
 			} else if (rand == 2) {
 				int capacity = new Random().nextInt(50);
 				Appliance appliance = new Refrigerator("Refrigerator" + i, "Coldg0" + i, price, capacity);
+				appliance.setQuantity(5);
 				shop.getAppliances().insertAppliance(appliance);
 			} else if (rand == 3) {
 				int HeatOutPut = new Random().nextInt(999999);
 				Appliance appliance = new Furnace("Furnace" + i, "HeatOne23" + i, price, HeatOutPut);
+				appliance.setQuantity(5);
 				shop.getAppliances().insertAppliance(appliance);
 			}
 		}
