7eea38b7ba5e6288d614d3e3297321e6d4051591
Tai Vu <vutai866@gmail.com>
2022-03-18 22:22:54 -0500

    Update ApplianceList

--- ApplianceList.java
+++ ApplianceList.java
@@ -7,7 +7,6 @@
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
-import java.util.Random;
 
 public class ApplianceList implements Iterable<Appliance> {
 	private List<Appliance> appliances = new ArrayList<>();
